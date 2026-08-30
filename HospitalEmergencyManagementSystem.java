c14b8b248e5f269216427882e0286df215da8f4d
Duneesha-weerasooriya <22ug3-0580@sltc.ac.lk>
2026-08-30 19:38:13 +0530

    Implement BST insertion for patient registration

--- HospitalEmergencyManagementSystem.java
+++ HospitalEmergencyManagementSystem.java
@@ -81,4 +81,39 @@ class PatientRecordBST {
     public PatientRecordBST() {
         root = null;
     }
-    
+
+    // --------------------------------------------------------
+    // Insert patient
+    // --------------------------------------------------------
+    public boolean insertPatient(Patient patient) {
+        if (searchPatient(patient.getPatientId()) != null) {
+            System.out.println("Patient ID already exists.");
+            return false;
+        }
+
+        root = insertRec(root, patient);
+        System.out.println("Patient registered successfully: "
+                + patient.getPatientName());
+
+        return true;
+    }
+
+    private BSTNode insertRec(BSTNode root, Patient patient) {
+
+        if (root == null) {
+            return new BSTNode(patient);
+        }
+
+        if (patient.getPatientId()
+                .compareTo(root.patient.getPatientId()) < 0) {
+
+            root.left = insertRec(root.left, patient);
+
+        } else if (patient.getPatientId()
+                .compareTo(root.patient.getPatientId()) > 0) {
+
+            root.right = insertRec(root.right, patient);
+        }
+
+        return root;
+    }
