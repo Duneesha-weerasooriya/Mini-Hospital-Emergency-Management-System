b474827d75e67716f03bf7386a7ee33cf4388c1a
Duneesha-weerasooriya <22ug3-0580@sltc.ac.lk>
2026-08-30 19:05:32 +0530

    Add Patient model class with patient details

--- HospitalEmergencyManagementSystem.java
+++ HospitalEmergencyManagementSystem.java
@@ -21,3 +21,34 @@ class Patient {
         this.contactNumber = contactNumber;
         this.medicalCondition = medicalCondition;
     }
+
+    // Getters
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getMedicalCondition() {
+        return medicalCondition;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + patientId +
+                ", Name: " + patientName +
+                ", Age: " + age +
+                ", Contact: " + contactNumber +
+                ", Condition: " + medicalCondition;
+    }
+}
